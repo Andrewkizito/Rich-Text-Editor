fcd9e36d3583fefd04f9d79ddc2f1e842c3517cb
Andrewkizito <Andrewkizito54@gmail.com>
2023-07-02 08:43:57 +0300

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Editor from "./components/Editor/main";
+
+function App() {
+  return <Editor />;
+}
+
+export default App;
---
+++ src/components/Editor/main.jsx
@@ -0,0 +1,107 @@
+// Importing helper modules
+import { useCallback, useMemo, useRef, useState } from "react";
+
+// Importing core components
+import QuillEditor from "react-quill";
+
+// Importing styles
+import "react-quill/dist/quill.snow.css";
+import styles from "./styles.module.css";
+
+const Editor = () => {
+  // Editor state
+  const [value, setValue] = useState("");
+
+  // Editor ref
+  const quill = useRef();
+
+  // Handler to handle button clicked
+  function handler() {
+    console.log(value);
+  }
+
+  const imageHandler = useCallback(() => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+
+    input.onchange = () => {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const imageUrl = reader.result;
+        const quillEditor = quill.current.getEditor();
+
+        const range = quillEditor.getSelection(true);
+        quillEditor.insertEmbed(range.index, "image", imageUrl);
+      };
+
+      reader.readAsDataURL(file);
+    };
+  }, []);
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [2, 3, 4, false] }],
+          ["bold", "italic", "underline", "blockquote"],
+          [{ color: [] }],
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+          ],
+          ["link", "image"],
+          ["clean"],
+        ],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+      clipboard: {
+        matchVisual: true,
+      },
+    }),
+    [imageHandler]
+  );
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "color",
+    "clean",
+  ];
+
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.label}>Editor Content</label>
+      <QuillEditor
+        ref={(el) => (quill.current = el)}
+        className={styles.editor}
+        theme="snow"
+        value={value}
+        formats={formats}
+        modules={modules}
+        onChange={(value) => setValue(value)}
+      />
+      <button onClick={handler} className={styles.btn}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default Editor;
